03adc9c17d7e25233caf57bafac5798b31262924
Tauana Pacheco <tauanapachecopp@gmail.com>
2024-10-29 23:36:11 -0300

    as the teacher

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import os
-from config import app
-from aluno.controller_aluno import alunos_blueprint
-from professor.controller_professor import professor_blueprint
-from turma.controller_turma import turma_blueprint
+from config import app, db
+from aluno.aluno_routes import alunos_blueprint
 
 app.register_blueprint(alunos_blueprint)
-app.register_blueprint(professor_blueprint)
-app.register_blueprint(turma_blueprint)
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
-  app.run(host=app.config["HOST"], port = app.config['PORT'], debug=app.config['DEBUG'] )
+  app.run(host=app.config["HOST"], port = app.config['PORT'],debug=app.config['DEBUG'] )
