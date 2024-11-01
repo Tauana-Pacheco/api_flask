c132f919fbf22d53d20be6166267c8a3ddce85db
Tauana Pacheco <tauanapachecopp@gmail.com>
2024-11-01 08:54:27 -0300

    feat: add db and routes in turmas

--- professor/prof_model.py
+++ professor/prof_model.py
@@ -9,7 +9,6 @@ class Professor(db.Model):
   observacoes = db.Column(db.String(80))
   turmas = db.relationship('Turma', backref='professor', lazy=True)
 
-
   def __init__(self, nome, idade, materia, observacoes, turmas):
     self.nome = nome
     self.idade = idade
@@ -26,3 +25,45 @@ class Professor(db.Model):
           'observacoes': self.observacoes,
           'turmas': [turma.to_dict() for turma in self.turmas]
         }
+
+class ProfessorNaoEncontrado(Exception):
+    pass
+
+def professor_por_id(id_professor):
+    professor = Professor.query.get(id_professor)
+    if not professor:
+        raise ProfessorNaoEncontrado
+    return professor.to_dict()
+
+def listar_professores():
+    professores = Professor.query.all()
+    return [professor.to_dict() for professor in professores]
+
+def adicionar_professor(professor_data):
+    novo_professor = Professor(
+        nome=professor_data['nome'], 
+        idade=professor_data['idade'],
+        materia=professor_data['materia'],
+        observacoes=professor_data['observacoes'],
+        turmas=professor_data['turmas'],
+        )
+    db.session.add(novo_professor)
+    db.session.commit()
+
+def atualizar_professor(id_professor, novos_dados):
+   professor = Professor.query.get(id_professor)
+   if not professor:
+      raise ProfessorNaoEncontrado
+   professor.nome = novos_dados['nome']
+   professor.idade = novos_dados['idade']
+   professor.materia = novos_dados['materia']
+   professor.observacoes = novos_dados['observacoes']
+   professor.turmas = novos_dados['turmas']
+   db.session.commit()
+
+def excluir_professor(id_professor):
+    professor = Professor.query.get(id_professor)
+    if not professor:
+        raise ProfessorNaoEncontrado
+    db.session.delete(professor)
+    db.session.commit()
--- turma/turma_model.py
+++ turma/turma_model.py
@@ -5,21 +5,55 @@ class Turma(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   descricao = db.Column(db.String(80))
   professor_id = db.Column(db.Integer, db.ForeignKey("professor.id"), nullable=False)
-  ativo = db.Column(db.Boolean)
+  ativo = db.Column(db.Boolean, default=True)
 
-  alunos = db.relationship('Aluno', backref='turma', lazy=True)
-
-  def __init__(self, descricao, professor_id, ativo, alunos):
+  def __init__(self, descricao, professor_id):
     self.descricao = descricao
     self.professor_id = professor_id
-    self.ativo = ativo
-    self.alunos = alunos
+   # self.ativo = ativo
 
   def to_dict(self):
       return {
           'id': self.id,
           'descricao': self.descricao,
           'professor_id': self.professor_id,
-          'ativo': self.ativo,
-          'alunos': [aluno.to_dict() for aluno in self.alunos]
+         # 'ativo': self.ativo,
       }
+
+class TurmaNaoEncontrado(Exception):
+    pass
+
+def turma_por_id(id_turma):
+    turma = Turma.query.get(id_turma)
+    if not turma:
+        raise TurmaNaoEncontrado
+    return turma.to_dict()
+
+def listar_turmas():
+   turmas = Turma.query.all()
+   return [turma.to_dict() for turma in turmas]
+
+def adicionar_turma(turma_data):
+    nova_turma = Turma( 
+        descricao=turma_data['descricao'],
+        professor_id=turma_data['professor_id'],
+      #  ativo=turma_data['ativo'],
+        )
+    db.session.add(nova_turma)
+    db.session.commit()
+
+def atualizar_turma(id_turma, novos_dados):
+    turma = Turma.query.get(id_turma)
+    if not turma:
+        raise TurmaNaoEncontrado
+    turma.descricao = novos_dados['descricao']
+    turma.professor_id = novos_dados['professor_id']
+    #turma.ativo = novos_dados['ativo']
+    db.session.commit()
+
+def excluir_turma(id_turma):
+    turma = Turma.query.get(id_turma)
+    if not turma:
+        raise TurmaNaoEncontrado
+    db.session.delete(turma)
+    db.session.commit()
